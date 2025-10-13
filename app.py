2f76f874686d3605f60c410d2f89cc64d44eb704
Ryanmh1024 <Ryanmh1024@users.noreply.github.com>
2025-10-13 18:49:25 -0400

    changed OLLAMA_URL so that it says 10.0.0.4 for port instead of 127.0.0.1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 
 # ----- TinyLlama via Ollama settings (Stage 2) -----
-OLLAMA_URL = os.getenv("OLLAMA_URL", "http://127.0.0.1:11434")
+OLLAMA_URL = os.getenv("OLLAMA_URL", "http://10.0.0.4:11434")
 OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "tinyllama")
 
 @app.get("/")
